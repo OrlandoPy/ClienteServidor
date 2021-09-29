cdad4745ae408e3e1124b90ab14f9cdffe4f7dca
OrlandoGomez <orlipy5@gmail.com>
2021-09-29 14:52:11 -0400

    Cliente-Servidor

---
+++ src/test/java/com/excelsis/ClienteServidor/ClienteServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelsis.ClienteServidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
